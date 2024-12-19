b6f0abbc11b0a4be8be67738b8f5f2f1a5bb234d
Seymur Orucov <seymur.o@bestcomp.net>
2024-12-19 18:22:53 +0400

    DDD

--- apps/account/tailwind.config.js
+++ apps/account/tailwind.config.js
@@ -1,9 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-import sharedConfig from '@repo/tailwind-config'
-
 export default {
-  // Spread the shared config
-  ...sharedConfig,
-  // Override content to only include current app's content
+  presets: ['@repo/tailwind-config'],
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
 }
--- packages/tailwind-config/index.js
+++ packages/tailwind-config/index.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     // Note: Changes here must be reflected in all apps and packages that use this config
-    '../../packages/ui/**/*.{js,ts,jsx,tsx}',
-    '../../apps/**/*.{js,ts,jsx,tsx}',
+    '*.{js,ts,jsx,tsx}',
+    '.*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
