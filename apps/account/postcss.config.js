4e47312c67f8cf4d7f6b425769914c0e222f2c98
Seymur Orucov <seymur.o@bestcomp.net>
2024-12-20 17:22:55 +0400

    tailwind

--- apps/account/postcss.config.js
+++ apps/account/postcss.config.js
@@ -1,4 +1,7 @@
+import sharedConfig from '@repo/shared/postcss.config'
+
 export default {
+  ...sharedConfig,
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
--- apps/account/tailwind.config.ts
+++ apps/account/tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from 'tailwindcss'
-import sharedConfig from '@repo/shared/tailwind-config'
+import sharedConfig from '@repo/shared/tailwind.config'
 
 export default {
   ...sharedConfig,
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 } satisfies Config
